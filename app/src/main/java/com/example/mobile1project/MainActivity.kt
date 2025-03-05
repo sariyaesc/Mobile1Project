2d8ff66d6dcae511c01a68e6a9f232cc77d52e4b
sariyaesc <escamillasara49@gmail.com>
2025-03-04 19:13:47 -0600

    change name

--- app/src/main/java/com/example/mobile1project/MainActivity.kt
+++ app/src/main/java/com/example/mobile1project/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             Mobile1ProjectTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Sara",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
